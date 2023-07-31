4549c7d74153b9d357305ef8f7a350bc9c762777
Sai Harshini Nimmala <quic_snimmala@quicinc.com>
2023-07-31 16:37:28 -0700

    ANDROID: tracing: Add restricted vendor hooks for preemptirq events
    This reverts commit d94fa9dcf5ba9d35ae76e6c25aaca7cc947eadb4.
    
    This commit reverts the revert, effectively bringing back the original
    change (Ic8d3cdd1c1aa6a9267d0b755694fedffa2ea8e36)
    
    Bug: 174541725
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: Sai Harshini Nimmala <quic_snimmala@quicinc.com>
    Change-Id: I24a3cdf17abf41e7512438516b2cfe561802c666

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -19,6 +19,7 @@
 #include <trace/hooks/printk.h>
 #include <trace/hooks/epoch.h>
 #include <trace/hooks/cpufreq.h>
+#include <trace/hooks/preemptirq.h>
 #include <trace/hooks/ufshcd.h>
 #include <trace/hooks/cgroup.h>
 #include <trace/hooks/sys.h>
@@ -52,6 +53,10 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_printk_hotplug);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_show_suspend_epoch_val);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_show_resume_epoch_val);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_freq_table_limits);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_preempt_disable);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_preempt_enable);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_irqs_disable);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_irqs_enable);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_cpu_cgroup_attach);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_cpu_cgroup_online);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_fill_prdt);
