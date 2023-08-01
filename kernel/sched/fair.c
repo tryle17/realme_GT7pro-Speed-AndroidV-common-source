c64826366c15b11864aac61be53f11e6e133497f
Sai Harshini Nimmala <quic_snimmala@quicinc.com>
2023-08-01 10:07:59 -0700

    ANDROID: Sched: Add vendor hooks required for WALT
    This reverts commit a7a5cf9f192a991a941d56492afcab08eafb2bbd.
    
    This commit reverts the revert, effectively bringing back the original
    change (I03327dc00d35e6fabcf8b5e254c4da548943d1a7).
    
    Bug: 200103201
    Bug: 243793188
    Signed-off-by: Ashay Jaiswal <quic_ashayj@quicinc.com>
    Signed-off-by: Sai Harshini Nimmala <quic_snimmala@quicinc.com>
    Change-Id: Ie74c75acfda9d8023de5021398418cd41acd37a1

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4640,7 +4640,10 @@ static inline int task_fits_cpu(struct task_struct *p, int cpu)
 
 static inline void update_misfit_status(struct task_struct *p, struct rq *rq)
 {
-	if (!sched_asym_cpucap_active())
+	bool need_update = true;
+
+	trace_android_rvh_update_misfit_status(p, rq, &need_update);
+	if (!sched_asym_cpucap_active() || !need_update)
 		return;
 
 	if (!p || p->nr_cpus_allowed == 1) {
@@ -4794,6 +4797,7 @@ place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 		se->vruntime = vruntime;
 	else
 		se->vruntime = max_vruntime(se->vruntime, vruntime);
+	trace_android_rvh_place_entity(cfs_rq, se, initial, &vruntime);
 }
 
 static void check_enqueue_throttle(struct cfs_rq *cfs_rq);
@@ -9290,6 +9294,7 @@ static void update_cpu_capacity(struct sched_domain *sd, int cpu)
 	if (!capacity)
 		capacity = 1;
 
+	trace_android_rvh_update_cpu_capacity(cpu, &capacity);
 	cpu_rq(cpu)->cpu_capacity = capacity;
 	trace_sched_cpu_capacity_tp(cpu_rq(cpu));
 
